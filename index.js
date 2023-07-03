2123c396ea24cade39ba651374b6693f247e9662
[SW5]정충래 <choongraej@gmail.com>
2023-07-03 10:05:29 +0000

    Revert "Merge branch 'feature-fe/footer' into 'master'"
    This reverts merge request !2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require("./src/app");
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PROT || 8080;
 
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
--- src/app.js
+++ src/app.js
@@ -1,11 +1,8 @@
 const express = require("express");
 const path = require("path");
 
-const { viewRouter } = require("./routes");
-
 const app = express();
 
 app.use("/", express.static(path.join(__dirname, "./views/public")));
-app.use("/", viewRouter);
 
 module.exports = app;
