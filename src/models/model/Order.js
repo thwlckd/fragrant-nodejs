6e86f47f6130da6b5e603dcafd5530c62bf50e2b
Park ChangHyub <thwlckd@naver.com>
2023-07-05 02:41:59 +0900

    Feat: OrderDAO 작성

--- src/models/model/Order.js
+++ src/models/model/Order.js
@@ -1,6 +1,44 @@
 const mongoose = require("mongoose");
 const { OrderSchema } = require("../schema");
-
 const Order = mongoose.model("Order", OrderSchema);
 
-module.exports = Order;
+const orderDAO = {
+  async create({ products, orderer, price, orderStatus, requirement }) {
+    const toCreate = {
+      products,
+      orderer,
+      price,
+      orderStatus,
+    };
+    if (requirement) toCreate.request = request;
+    const order = await Order.create(toCreate);
+    return order.toObject();
+  },
+
+  async findOne(orderId) {
+    const order = await Order.findById({ _id: orderId }).lean();
+    return order;
+  },
+
+  async findAll() {
+    const order = await Order.find({}).lean();
+    return order;
+  },
+
+  async findAllByUserEmail(userEmail) {
+    const order = await Order.find({ orderer: { email: userEmail } }).lean();
+    return order;
+  },
+
+  async updateOne(id, toUpdate) {
+    const order = await Order.findByIdAndUpdate({ _id: orderId }, toUpdate);
+    return order;
+  },
+
+  async deleteOne(id) {
+    const order = await order.findByIdAndDelete({ _id: orderId }).lean();
+    return order;
+  },
+};
+
+module.exports = orderDAO;
