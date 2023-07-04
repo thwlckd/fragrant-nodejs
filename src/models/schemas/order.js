b967b17ef87403c09a975bc6f3c8af03506fb3ac
Park ChangHyub <thwlckd@naver.com>
2023-07-05 02:45:26 +0900

    Change: Order 스키마 구조 변경 - products, orderer 서브스키마 사용

--- src/models/schemas/order.js
+++ src/models/schemas/order.js
@@ -1,29 +1,50 @@
 const { Schema } = required("mongoose");
-const shortId = required("./types/shortId");
 
 const OrderSchema = new Schema(
   {
-    orderId: {
-      type: String,
-      default: shortId(),
-      required: true,
-      unique: true,
-      index: true,
+    products: [
+      {
+        productId: {
+          type: String,
+          required: true,
+        },
+        name: {
+          type: String,
+          required: true,
+        },
+        price: {
+          type: Number,
+          requried: true,
+        },
+        img: {
+          type: String,
+          requried: true,
+        },
+        capacity: {
+          type: Number,
+          requried: true,
+        },
+      },
+    ],
+    orderer: {
+      email: {
+        type: String,
+        required: true,
+      },
+      phone: {
+        type: String,
+        required: true,
+      },
+      name: {
+        type: String,
+        required: true,
+      },
+      address: {
+        type: String,
+        required: true,
+      },
     },
     price: {
-      type: Number,
-      required: true,
-    },
-    orderTime: {
-      type: Date,
-      default: Date.now(),
-      required: true,
-    },
-    address: {
-      type: String,
-      required: true,
-    },
-    phone: {
       type: Number,
       required: true,
     },
@@ -31,12 +52,8 @@ const OrderSchema = new Schema(
       type: String,
       required: true,
     },
-    request: {
+    requirement: {
       type: String,
-    },
-    productId: {
-      type: Array,
-      requied: true,
     },
   },
   {
