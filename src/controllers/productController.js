d8d24c6656e3cf408b1c9243114cf912d6dfb2b7
CHOONGRAE <choongraej@gmail.com>
2023-07-05 13:48:53 +0900

    Feat: productController 추가
    productController 구현, 최소기능으로 생각되는 부분만 만들었습니다, 필요한 기능이 생길시 추가하겠습니다.

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -2,7 +2,11 @@ const productService = require("../services/productService");
 
 const productController = {
   // 전체 상품
-  async getAllProduct(req, res, next) {},
+  async getAllProduct(req, res) {
+    const products = await productService.getAllProduct();
+
+    res.status(200).json({ products });
+  },
 };
 
 module.exports = productController;
