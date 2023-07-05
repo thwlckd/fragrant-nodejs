d19dc5bd316b92a8e097d69d450de7887bb6bec3
Park ChangHyub <thwlckd@naver.com>
2023-07-06 02:00:23 +0900

    Change: userRouter endpoint 변경
    동일한 이름의 유저 리스트 조회 -> /users/userName?userName

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -6,7 +6,7 @@ const userRouter = Router();
 
 userRouter.get("/:userId", loginRequired, userController.getUser);
 userRouter.get("/", adminOnly, userController.getUsers);
-userRouter.get("/?userName", adminOnly, userController.getUsersByUserName);
+userRouter.get("/userName", adminOnly, userController.getUsersByUserName);  
 userRouter.patch("/:userId", loginRequired, userController.patchUser);
 userRouter.delete("/:userEmail", adminOnly, userController.deleteUserByEmail);
 userRouter.delete(
