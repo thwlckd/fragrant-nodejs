ff57feb0a6a8a6aada5bd1f93ef80ef2da26ac99
CHOONGRAE <choongraej@gmail.com>
2023-07-08 01:19:53 +0900

    Change: 서버 작업중

--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -1,5 +1,10 @@
 const { Router } = require('express');
+const productController = require('../controllers/productController');
+const asyncHandler = require('../middlewares/asyncHandler');
 
 const productRouter = Router();
+
+productRouter.get('/', asyncHandler(productController.getAllProducts));
+productRouter.get('/:productId', asyncHandler(productController.getProductByProductId));
 
 module.exports = productRouter;
