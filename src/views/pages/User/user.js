7656c27e66478f0540cca20eaca90fb3371f71e6
[SW5] 이수민 <tnklr1111@gmail.com>
2023-07-09 02:12:03 +0900

    Change: eslint 적용 후 코드 구문 수정

--- src/views/pages/User/user.js
+++ src/views/pages/User/user.js
@@ -1,17 +1,17 @@
 async function getOrderList() {
-  const orders = await fetch("/orders/user", {
-    method: "GET",
+  const orders = await fetch('/orders/user', {
+    method: 'GET',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
       Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyRW1haWwiOiJ0ZXN0QGdtYWlsLmNvbSIsImlzQWRtaW4iOmZhbHNlLCJpYXQiOjE2ODg4MjY2OTJ9.h30XrRfX7cAuwfvGgyxB5e6DTB-WLaCeQVbhH_NaXwQ",
+        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyRW1haWwiOiJ0ZXN0QGdtYWlsLmNvbSIsImlzQWRtaW4iOmZhbHNlLCJpYXQiOjE2ODg4MjY2OTJ9.h30XrRfX7cAuwfvGgyxB5e6DTB-WLaCeQVbhH_NaXwQ',
     },
   });
+  let result;
   if (orders.ok) {
-    return orders.json();
-  } else {
-    throw new Error("Failed.");
+    result = await orders.json();
   }
+  return result;
 }
 
 async function displayOrderList() {
@@ -20,32 +20,31 @@ async function displayOrderList() {
   console.log(orderList);
 
   // 주문 내역 데이터를 리스트에 추가
-  const orderListDiv = document.getElementById("order-history-list");
+  const orderListDiv = document.getElementById('order-history-list');
 
-  for (let i = 0; i < orderList.length; i++) {
+  for (let i = 0; i < orderList.length; i += 1) {
     const orderItem = orderList[i];
-    const products = orderItem.products;
+    const { products } = orderItem;
 
-    const paymentUnit = orderItem.price
-      .toString()
-      .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    const paymentUnit = orderItem.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 
     let productName = products[0].name;
     if (products.length > 1) {
-      productName += " 외 " + (products.length - 1) + " 건";
+      productName += ` 외 ${products.length - 1} 건`;
     }
 
-    let listItem = document.createElement("li");
+    const listItem = document.createElement('li');
+    const { createdAt, _id, orderStatus } = orderItem;
     listItem.innerHTML = `<div class="order-list"> 
       <div class="order-header">
-        <p class="order-date">${orderItem.createdAt}</p>
+        <p class="order-date">${createdAt}</p>
         <a class="view-order-detail" href="../orders/:orderId">
           주문내역 상세보기
         </a>
       </div>
       <div class="order-list-1">
         <div class="order-list-component">
-          <a href="/products/:productId">
+          <a href="/products/:productId"></a>
             <img
               class="product-image"
               src="/asset/perfume/red_elice.png"
@@ -55,11 +54,11 @@ async function displayOrderList() {
           <div class="order-info">
             <div class="order-contents-value">상품명<span>${productName}</span>
       </div>
-            <div class="order-contents-value">주문번호<span>${orderItem._id}</div>
+            <div class="order-contents-value">주문번호<span>${_id}</div>
             <div class="order-contents-value">결제금액<span>${paymentUnit} 원</span></div>
           </div>
           <div class="order-status-box">
-            <p class="order-status">${orderItem.orderStatus}</p>
+            <p class="order-status">${orderStatus}</p>
             <button type="button"  class="order-cancel">
               주문취소
             </button>
