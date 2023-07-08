6a1e8673dc9784db10f889f721a7c0133e3faa4d
[SW5] 임소정 <sojung4166@gmail.com>
2023-07-08 23:41:26 +0900

    Feat:로그인 API구현중

--- src/views/pages/Login/login.js
+++ src/views/pages/Login/login.js
@@ -1,17 +1,38 @@
 console.log("확인");
 
+//const email = document.getElementById("uid").value;
+const idEmail = document.querySelector("#uid");
+const password = document.querySelector("#upassword");
+const loginForm = document.querySelector(".userinput");
+
+loginForm.addEventListener("submit", e => {
+  e.preventDefault()
+
+  fetch("/auth/sign-in", {
+    method: "POST",
+    body: JSON.stringify({
+      email: idEmail.value,
+      password: password.value
+    }),
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      console.log(res);
+      localStorage.setItem("token", res.token);
+    });
+})
+
 
 // 아이디 입력 유효성
-const idEmail = document.querySelector("#uid");
+
 const idEmailError = document.querySelector("#uid+.login-msg");
 
 const idRegExp =
   /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
 
-
 const idEmailErrorMsg = {
   0: "이메일을 입력하세요",
-  1: "아이디(이메일)는 이메일 형식으로 입력해주세요.."
+  1: "아이디(이메일)는 이메일 형식으로 입력해주세요.",
 };
 
 console.log(idEmailError);
