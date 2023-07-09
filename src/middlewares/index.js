b52fc760bafe79b6a4d7acf876185e16833d96f7
Park ChangHyub <thwlckd@naver.com>
2023-07-10 05:26:31 +0900

    Refator: 이메일 중복 service 단에서 처리

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,11 +1,9 @@
 const adminOnly = require('./adminOnly');
 const loginRequired = require('./loginRequired');
 const asyncHandler = require('./asyncHandler');
-const checkDuplicatedEmail = require('./checkDuplicatedEmail');
 
 module.exports = {
   adminOnly,
   loginRequired,
   asyncHandler,
-  checkDuplicatedEmail,
 };
--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,10 +1,9 @@
 const { Router } = require('express');
 const { userController } = require('../controllers');
-const { checkDuplicatedEmail } = require('../middlewares');
 
 const authRouter = Router();
 
-authRouter.post('/sign-up', checkDuplicatedEmail, userController.postSignUpInfo);
+authRouter.post('/sign-up', userController.postSignUpInfo);
 authRouter.post('/sign-in', userController.postSignInInfo);
 
 module.exports = authRouter;
