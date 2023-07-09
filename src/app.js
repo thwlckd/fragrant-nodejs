ae2c5cc796471d779e568b567bfb6dff85e1da67
Park ChangHyub <thwlckd@naver.com>
2023-07-10 04:11:25 +0900

    Feat: passport strategies를 이용한 인증 방법 변경
    - LocalStrategy 작성
    - JwtStrategy 작성
    - 서버에서 클라이언트 쿠키에 토큰 심어줌
    - getToken 미들웨어로 모든 요청에 대한 토큰을 받도록 설정

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const passport = require('passport');
+const { getToken } = require('./middlewares');
 const { router } = require('./routes');
 
 const { viewRouter } = require('./routes');
 
 require('dotenv').config();
+require('./passport')();
 
 const { MONGODB_ADDRESS } = process.env;
 
@@ -13,6 +17,9 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser()); // cookie-parser는 요청과 함께 들어온 쿠키를 해석하여 req.cookies객체로 만들어줌
+app.use(passport.initialize());
+app.use(getToken);
 
 app.use('/', express.static(path.join(__dirname, './views/public')));
 app.use('/', viewRouter);
@@ -27,7 +34,7 @@ app.use('/', router);
 
 app.use((error, req, res, next) => {
   console.log(error);
-  next();
+  res.status(400).end()
 });
 
 module.exports = app;
