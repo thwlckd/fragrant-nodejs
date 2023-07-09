bbf57d7cc5a5b4a151747c877ab7d1496f705006
[SW5] 이수민 <tnklr1111@gmail.com>
2023-07-10 04:55:07 +0900

    Feat: 주문상세내역 퍼블리싱 추가 진행
    - 배송 현황 표시 영역에 svg 파일을 적용했습니다.
    - 주문취소 모달을 추가했습니다.
    - 그 외 일부 디자인을 수정했습니다.

--- src/views/pages/OrderDetail/orderDetail.js
+++ src/views/pages/OrderDetail/orderDetail.js
@@ -1,2 +1,15 @@
 const payment = document.querySelector('#payment-amount');
-payment.innerHTML = '총 결제금액 | 000,000원';
+payment.innerHTML = `총 결제금액 | 000,000원`;
+
+const open = () => {
+  document.querySelector('.modal').classList.remove('hidden');
+};
+
+const close = () => {
+  document.querySelector('.modal').classList.add('hidden');
+};
+
+document.querySelector('.order-cancel-btn').addEventListener('click', open);
+document.querySelector('.close-btn').addEventListener('click', close);
+document.querySelector('.background').addEventListener('click', close);
+// document.gquerySelector('modal').scrollTo(0, 0);
