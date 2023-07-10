6fa646c4546bc6f90e9b58ef379017096b337c6a
CHOONGRAE <choongraej@gmail.com>
2023-07-10 11:41:11 +0900

    Feat: note 컨트롤러 구현
    note 컨트롤러 기능 구현

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,10 +2,12 @@ const orderController = require('./orderController');
 const userController = require('./userController');
 const productController = require('./productController');
 const brandController = require('./brandController');
+const noteController = require('./noteController');
 
 module.exports = {
   orderController,
   productController,
   userController,
   brandController,
+  noteController,
 };
