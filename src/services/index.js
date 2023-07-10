aad5a1fd81f61da41abd8a3a1e6f0fe0836ea040
CHOONGRAE <choongraej@gmail.com>
2023-07-10 11:39:55 +0900

    Feat: note 서비스 구현
    note 서비스 기능 구현

--- src/services/index.js
+++ src/services/index.js
@@ -2,10 +2,12 @@ const orderService = require('./orderService');
 const userService = require('./userService');
 const productService = require('./productService');
 const brandService = require('./brandService');
+const noteService = require('./noteService');
 
 module.exports = {
   orderService,
   userService,
   productService,
   brandService,
+  noteService,
 };
