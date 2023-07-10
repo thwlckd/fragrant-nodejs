96ede36bbbc8b13666002c9f387e0c6f8a0b3848
CHOONGRAE <choongraej@gmail.com>
2023-07-10 11:45:25 +0900

    Change: noteDAO 반환타입 수정
    오탈자 수정

--- src/models/model/Note.js
+++ src/models/model/Note.js
@@ -3,46 +3,50 @@ const { NoteSchema } = require('../schemas');
 
 const Note = mongoose.model('Note', NoteSchema);
 
-const NoteDAO = {
+const noteDAO = {
   async getAllNotes() {
-    const notes = await Note.find({});
+    const notes = await Note.find({}).lean();
 
     return notes;
   },
 
   async getNoteByNoteId(noteId) {
-    const note = await Note.findOne({ _id: noteId });
+    const note = await Note.findOne({ _id: noteId }).lean();
 
     return note;
   },
 
   async getNoteByNoteType(noteType) {
-    const note = await Note.findOne({ type: noteType });
+    const note = await Note.findOne({ type: noteType }).lean();
 
     return note;
   },
 
-  async createNote({ noteType }) {
-    const note = await Note.create({ type: noteType });
+  async createNote({ type }) {
+    const note = await Note.create({ type });
 
     return note;
   },
 
   async updateNoteByNoteId(noteId, updateInfo) {
-    await Note.findByIdAndUpdate(noteId, updateInfo);
+    await Note.findByIdAndUpdate(noteId, updateInfo).lean();
   },
 
   async updateNoteByNoteType(noteType, updateInfo) {
-    await Note.findOneAndUpdate({ type: noteType }, updateInfo);
+    await Note.findOneAndUpdate({ type: noteType }, updateInfo).lean();
   },
 
   async deleteNoteByNoteId(noteId) {
-    await Note.deleteOne({ _id: noteId });
+    const { deletedCount } = await Note.deleteOne({ _id: noteId }).lean();
+
+    return deletedCount;
   },
 
   async deleteNoteByNoteType(noteType) {
-    await Note.deleteOne({ type: noteType });
+    const { deletedCount } = await Note.deleteOne({ type: noteType }).lean();
+
+    return deletedCount;
   },
 };
 
-module.exports = NoteDAO;
+module.exports = noteDAO;
