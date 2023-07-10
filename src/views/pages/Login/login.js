71f8a63babd60298ac9fad217d8027345e2dffc4
[SW5] 임소정 <sojung4166@gmail.com>
2023-07-10 23:46:19 +0900

    Hotfix: redirect 안되는거 해결

--- src/views/pages/Login/login.js
+++ src/views/pages/Login/login.js
@@ -1,6 +1,3 @@
-console.log('확인');
-
-// const email = document.getElementById("uid").value;
 const idEmail = document.querySelector('#uid');
 const password = document.querySelector('#upassword');
 const loginForm = document.querySelector('.userinput');
@@ -17,18 +14,17 @@ loginForm.addEventListener('submit', (e) => {
       email: idEmail.value,
       password: password.value,
     }),
-  })
-    .then((res) => {
-      console.log(res);
-      // localStorage.setItem('token', res.token);
-      if (!res.ok) {
-        alert('아이디(이메일) 또는 비밀번호를 확인해주세요.');
-       }
-    });
+  }).then((res) => {
+    console.log(res);
+    if (!res.ok) {
+      alert('아이디(이메일) 또는 비밀번호를 확인해주세요.');
+    } else {
+      window.location.href = res.url;
+    }
+  });
 });
 
 // 아이디 입력 유효성
-
 const idEmailError = document.querySelector('#uid+.login-msg');
 
 const idRegExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
@@ -44,7 +40,6 @@ idEmail.oninput = () => {
   const { error0, error1 } = idEmailErrorMsg;
 
   if (idEmail.value !== '') {
-    console.log(idRegExp.test(idEmail.value));
     if (idRegExp.test(idEmail.value)) {
       idEmailError.textContent = '';
       idEmailError.style.display = 'none';
@@ -64,7 +59,6 @@ const loginBtn = document.querySelector('#sbtn');
 loginBtn.addEventListener('click', (e) => {
   if (!(idEmail.value !== '' && idEmailError.textContent === '' && password.value !== '')) {
     e.preventDefault();
-    console.log('입력해');
     alert('아이디(이메일), 비밀번호를 확인해주세요.');
   }
 });
