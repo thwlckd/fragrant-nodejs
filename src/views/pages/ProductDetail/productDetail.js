b9f0b11fa561c28aaaaf3e58bc9375d9975e9ec6
왕지은 <jieunwang00@gmail.com>
2023-07-10 15:38:50 +0900

    Feat: 수량 증감 버튼 구현
    상품 수량 증감 버튼의 html, js파일을 구현했고 css는 변경 예정입니다.

--- src/views/pages/ProductDetail/productDetail.js
+++ src/views/pages/ProductDetail/productDetail.js
@@ -1,25 +1,33 @@
-// 상품 수량 늘리는 버튼 아직 미완!ㅂ
-function count(type) {
-  const resultElement = document.getElementsByClassName('num');
 
-  let number = resultElement.textContent;
-  number = parseInt(number, 10);
-
-  if (type === 'plus') {
-    number += 1;
-  } else if (type === 'minus') {
-    number -= 1;
+// 밑이 수량 증감 수정본. 수량 증감에 따른 판매가가 보여짐
+const plus = document.querySelector('.plus');
+const minus = document.querySelector('.minus');
+const result = document.querySelector('#result');
+const totalCost = document.querySelector('.totalCost');
+let i = 1;
+plus.addEventListener("click", () => {
+  i += 1
+  result.textContent = i;
+  const totalCostNum = i * 200000;
+  totalCost.textContent = `₩${  totalCostNum.toLocaleString()}`;
+})
+minus.addEventListener('click', () => {
+  if (i > 0) {
+  i -= 1;
+  result.textContent = i;
+  const totalCostNum = i * 200000;
+  totalCost.textContent = `₩${  totalCostNum.toLocaleString()}`;
+  } else {
+    totalCost.textContent = `₩${  0}`; // 판매가
   }
+});
 
-  resultElement.textContent = number;
-}
+// 장바구니에 넣었습니다라는 alert 창 띄우기, 이미 장바구니에 담긴 거면 이미 장바구니에 담겨있습니다라는 alert 창
 
-// 장바구니버튼 눌렀을때 장바구니로 이동,
-// 구매하기버튼 눌렀을 때 결제하는 곳으로 이동
-// 둘 다 로그인했는지 체크하고 false시 alert()팝업창 띄우기
+// 구매하기 버튼 눌렀을 때 구매창 url 받아서 이동
 
-// 리뷰 작성칸/현재 시간(연월일 시분), username, 댓글 최솟값설정10자, 등록하면 리뷰창에 올라가기
-// 별점
-const drawStar = (target) => {
-  document.querySelector('.star span').style.width = `${target.value * 10}%`;
-};
+
+// 별점 구현
+
+// 리뷰 업로드 함수.
+
