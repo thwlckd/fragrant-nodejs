3a557a48d4373ae1aa26b6bac1ba88244e9c2049
[SW5] 이수민 <tnklr1111@gmail.com>
2023-07-11 19:28:04 +0900

    Feat: api 연동 작업
    -  주문상세내역, 주문상품리스트품, 결제정보, 배송지정보 JS 코드를 작성함

--- src/views/pages/OrderDetail/orderDetail.js
+++ src/views/pages/OrderDetail/orderDetail.js
@@ -1,6 +1,77 @@
-const payment = document.querySelector('#payment-amount');
-payment.innerHTML = `총 결제금액 | 000,000원`;
+import { $, $create, $append } from '/js/util/dom.js';
 
+async function getProductList() {
+  const orders = await fetch('/orders/64a9d6ee08c6c74306d2c606', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  let result;
+  if (orders.ok) {
+    result = await orders.json();
+  }
+  return result;
+}
+
+async function displayProductList() {
+  const orderList = await getProductList();
+  const prodcutList = await orderList.products;
+
+  console.log(orderList);
+  console.log(prodcutList);
+
+  const { orderTime, _id, orderStatus, price, orderer, requirement } = orderList;
+  const paymentUnit = price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+  $('#order-date').textContent = `주문일자:\u00a0\ ${orderTime}`;
+  $('#order-id').textContent = `주문번호:\u00a0\ ${_id}`;
+  $('#order-status-text').textContent = orderStatus;
+
+  $('#payment-amount').textContent = `총 결제금액\u00a0\ | \u00a0\ ${paymentUnit} 원`;
+  $('#payment-type').textContent = `상품금액 ${paymentUnit} 원 + 배송비 0 원`;
+
+  $('#name').textContent = orderer.name;
+  $('#contact').textContent = orderer.phone;
+  $('#address').textContent = `${orderer.address.address1} \u00a0\ ${orderer.address.address2}`;
+  $('#requirement').textContent = requirement;
+
+  const $productListSection = $('#product-list');
+
+  for (let i = 0; i < prodcutList.length; i += 1) {
+    const productItem = prodcutList[i];
+    const { name, capacity, price, img, productId } = productItem;
+    const productPaymentUnit = price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+    const $liElement = $create('li', '');
+
+    const $orderProductList = $create('div', 'products-list');
+    $liElement.append($orderProductList);
+
+    const $imgLink = $create('a', 'img-link', { href: `/products/${productId}` });
+    const $productInfo = $create('div', 'product-info');
+
+    $append($orderProductList, $imgLink, $productInfo);
+
+    const $productImg = $create('img', 'product-image');
+
+    const $productName = $create('div', 'product-name');
+    $productName.textContent = name;
+    const $productCapa = $create('div', 'product-capa');
+    $productCapa.textContent = `${capacity} ml`;
+    const $productPrcie = $create('div', 'product-price');
+    $productPrcie.textContent = `${productPaymentUnit} 원 | 1개`;
+
+    $imgLink.append($productImg);
+
+    $append($productInfo, $productName, $productCapa, $productPrcie);
+
+    $append($productListSection, $liElement);
+  }
+}
+displayProductList();
+
+//주문취소 모달
 const open = () => {
   document.querySelector('.modal').classList.remove('hidden');
 };
@@ -9,7 +80,6 @@ const close = () => {
   document.querySelector('.modal').classList.add('hidden');
 };
 
-document.querySelector('.order-cancel-btn').addEventListener('click', open);
+document.querySelector('#order-cancel-btn').addEventListener('click', open);
 document.querySelector('.close-btn').addEventListener('click', close);
 document.querySelector('.background').addEventListener('click', close);
-// document.gquerySelector('modal').scrollTo(0, 0);
