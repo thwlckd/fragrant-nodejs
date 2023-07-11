1a89abc253179fc2a07221cde7a2d428c40c969d
Park ChangHyub <thwlckd@naver.com>
2023-07-12 01:57:16 +0900

    Refactor: asyncHandler 함수 처리

--- src/routes/orderRouter.js
+++ src/routes/orderRouter.js
@@ -1,20 +1,17 @@
 const { Router } = require('express');
 const { orderController } = require('../controllers');
 const { adminOnly, loginRequired } = require('../middlewares');
+const asyncHandler = require('../middlewares/asyncHandler');
 
 const orderRouter = Router();
 
-orderRouter.get('/', adminOnly, orderController.getOrders);
-orderRouter.get('/userName', adminOnly, orderController.getOrdersByUserName);
-orderRouter.post('/order', loginRequired, orderController.postOrder);
-orderRouter.get(
-  '/user',
-  loginRequired,
-  orderController.getOrdersByUserEmail,
-);
-orderRouter.get('/:orderId', loginRequired, orderController.getOrder);
-orderRouter.patch('/user/:orderId', loginRequired, orderController.patchUserOrder);
-orderRouter.patch('/admin/:orderId', adminOnly, orderController.patchAdminOrder);
-orderRouter.delete('/:orderId', loginRequired, orderController.deleteOrder);
+orderRouter.get('/', adminOnly, asyncHandler(orderController.getOrders));
+orderRouter.get('/userName', adminOnly, asyncHandler(orderController.getOrdersByUserName));
+orderRouter.post('/order', loginRequired, asyncHandler(orderController.postOrder));
+orderRouter.get('/user', loginRequired, asyncHandler(orderController.getOrdersByUserEmail));
+orderRouter.get('/:orderId', loginRequired, asyncHandler(orderController.getOrder));
+orderRouter.patch('/user/:orderId', loginRequired, asyncHandler(orderController.patchUserOrder));
+orderRouter.patch('/admin/:orderId', adminOnly, asyncHandler(orderController.patchAdminOrder));
+orderRouter.delete('/:orderId', loginRequired, asyncHandler(orderController.deleteOrder));
 
 module.exports = orderRouter;
