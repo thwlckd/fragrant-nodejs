ffdc3811b9f52ede065344b26165704207239727
왕지은 <jieunwang00@gmail.com>
2023-07-12 20:38:09 +0900

    Feat: 수량 증가 버튼의 기능 구현
    수량 증감 값에 상품 가격을 곱하는 기능을 추가하였습니다.

--- src/views/pages/ProductDetail/productDetail.js
+++ src/views/pages/ProductDetail/productDetail.js
@@ -1,33 +1,87 @@
+// 페이지 상품에 들어갈 경로 설정 . 이미지 브랜드명 상품명
+
+import { $ } from '/js/util/dom.js';
+
+let sum;
+const productDetailRenderer = async (url) => {
+  const {
+    product: {
+      name: { origin, korean },
+      capacity,
+      price,
+      picture,
+      note,
+    brand: {
+        name: { origin: brandorigin },
+      },
+      description,
+    },
+  } = await fetch(`/api${url}`).then((res) => res.json());
+  const $krNameElementTitle = $('.name-kr');
+  $krNameElementTitle.textContent = korean;
+
+  const $krNameElementDetail = $('#tab2 .kr-name-detail');
+  $krNameElementDetail.textContent = korean;
+
+  const $originNameElementTitle = $('.eng_name .name-origin');
+  $originNameElementTitle.textContent = origin;
+
+  const $originNameElementSub = $('.delivery_box .name-origin');
+  $originNameElementSub.textContent = origin;
+
+  const $capacityElement = $('.capacity');
+  $capacityElement.textContent = capacity;
+
+  const $priceElement = $('.totalCost');
+  $priceElement.textContent = price; // 원가, 숫자타입이엇음
+
+  const $pictureElement = $('.product_info img');
+  $pictureElement.src = picture;
+
+  const $noteElement1 = $('.note1');
+  $noteElement1.textContent = note[0].type;
+
+  const $noteElement2 = $('.note2');
+  $noteElement2.textContent = note[1].type;
+
+  const $noteElement3 = $('.note3');
+  $noteElement3.textContent = note[2].type;
+
+  const $brandNameElement = $('.eng_name .brandname-origin');
+  $brandNameElement.textContent = brandorigin;
+
+  const $descriptionElement = $('.description');
+  $descriptionElement.textContent = description;
+
+  sum = price;
+};
+const { pathname } = window.location;
+productDetailRenderer(pathname);
+
 
 // 밑이 수량 증감 수정본. 수량 증감에 따른 판매가가 보여짐
 const plus = document.querySelector('.plus');
 const minus = document.querySelector('.minus');
 const result = document.querySelector('#result');
 const totalCost = document.querySelector('.totalCost');
-let i = 1;
-plus.addEventListener("click", () => {
-  i += 1
+
+let i = 0;
+
+plus.addEventListener('click', () => {
+  i += 1;
   result.textContent = i;
-  const totalCostNum = i * 200000;
-  totalCost.textContent = `₩${  totalCostNum.toLocaleString()}`;
-})
-minus.addEventListener('click', () => {
-  if (i > 0) {
-  i -= 1;
-  result.textContent = i;
-  const totalCostNum = i * 200000;
-  totalCost.textContent = `₩${  totalCostNum.toLocaleString()}`;
-  } else {
-    totalCost.textContent = `₩${  0}`; // 판매가
-  }
+  const totalCostNum = i * sum;
+  totalCost.textContent = `\u00a0 ${totalCostNum.toLocaleString()}`;
 });
 
-// 장바구니에 넣었습니다라는 alert 창 띄우기, 이미 장바구니에 담긴 거면 이미 장바구니에 담겨있습니다라는 alert 창
-
-// 구매하기 버튼 눌렀을 때 구매창 url 받아서 이동
-
-
-// 별점 구현
-
-// 리뷰 업로드 함수.
+minus.addEventListener('click', () => {
+  if (i > 0) {
+    i -= 1;
+    result.textContent = i;
+    const totalCostNum = i * sum;
+    totalCost.textContent = `\u00a0 ${totalCostNum.toLocaleString()}`;
+  } else {
+    totalCost.textContent = `\u00a0 ${0}`; // 판매가
+  }
+});
 
