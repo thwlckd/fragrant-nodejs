2935e3c1050975acfdd6923a18b93b86526ec6bc
CHOONGRAE <choongraej@gmail.com>
2023-07-15 03:27:27 +0900

    서버 진입 변경

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const app = require('./src/app');
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server start on -> http://localhost:${PORT}`);
 });
